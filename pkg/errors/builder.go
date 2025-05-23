ac38c5420af894edfd5f3595b261f2826028460e
Ricky004 <tridipdam001@gmail.com>
2025-05-23 11:36:30 +0530

    feat(errors): add rich error builder with slog logging support

--- pkg/errors/builder.go
+++ pkg/errors/builder.go
@@ -1,1 +1,81 @@
 package errors
+
+import (
+	"errors"
+	"fmt"
+	"log/slog"
+)
+
+// Error interface compatibility
+func (e *Error) Error() string {
+	if e.Cause != nil {
+		return e.Cause.Error()
+	}
+
+	return fmt.Sprintf("%s[%s]: %s", e.Code, e.Severity, e.Message)
+}
+
+// Unwrap for 'errors.Is/As'
+func (e *Error) Unwrap() error {
+	return e.Cause
+}
+
+
+func (e *Error) LogValue() slog.Value {
+	return slog.GroupValue(
+		slog.String("code", string(e.Code)),
+		slog.String("severity", string(e.Severity)),
+		slog.String("message", e.Message),
+		slog.Any("meta", e.Meta),
+	)
+}
+
+
+// New creates a new structured error
+func New(code Code, message string, severity Severity, cause ...error) *Error {
+	var root error
+	if len(cause) > 0 {
+		root = cause[0]
+	}
+	return &Error{
+		Code:     code,
+		Message:  message,
+		Severity: severity,
+		Cause: root,
+	}
+}
+
+// NewMeta includes structured meta data
+func NewMeta(code Code, message string, severity Severity, meta map[string]any, cause ...error) *Error {
+	var root error
+	if len(cause) > 0 {
+		root = cause[0]
+	}
+	return &Error{
+		Code:     code,
+		Message:  message,
+		Severity: severity,
+		Cause:    root,
+		Meta:     meta,
+	}
+}
+
+func Wrap(err error, code Code, message string, severity Severity) *Error {
+	var e *Error
+	if errors.As(err, &e) {
+		return &Error{
+			Code: code,
+			Message: message,
+			Severity: severity,
+			Cause: e,
+			Meta: e.Meta,
+		}
+	} 
+    
+	return &Error{
+		Code:     code,
+		Message:  message,
+		Severity: severity,
+		Cause:    err,
+	}
+}
--- pkg/errors/errors.go
+++ pkg/errors/errors.go
@@ -1,4 +1,10 @@
 package errors
 
 
-
+type Error struct {
+	Code     Code           `json:"code"`
+	Message  string         `json:"message"`
+	Severity Severity       `json:"severity"`
+	Cause    error          `json:"-"`
+	Meta     map[string]any `json:"meta,omitempty"`
+}
